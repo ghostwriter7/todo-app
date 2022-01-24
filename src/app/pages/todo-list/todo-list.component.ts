0466f0bc98ab9495b4ca581727b22dde87760781
Hubert Rozplochowski <Hubert.Rozplochowski@createctrl.com>
2022-01-24 15:52:45 +0100

    add drag and drop

--- src/app/pages/todo-list/todo-list.component.ts
+++ src/app/pages/todo-list/todo-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Renderer2 } from '@angular/core';
 import { ITodoItem } from '../../core/interfaces';
 
 @Component({
@@ -18,6 +18,8 @@ export class TodoListComponent implements OnInit {
     {content: 'Live to the fullest', isActive: true},
     {content: 'Have a cup of cocoa', isActive: true}
   ];
+
+  constructor(private renderer: Renderer2) {}
 
   ngOnInit(): void {
     this.countLeftTodos();
@@ -40,6 +42,33 @@ export class TodoListComponent implements OnInit {
     this.filteringMode = 'All';
     this.mockup = this.mockup.filter(todo => todo.isActive);
     this.countLeftTodos();
+  }
+
+  public onDragEnter(todo: HTMLElement): void {
+    this.renderer.setStyle(todo, 'opacity', 0.7);
+  }
+
+  public onDragLeave(todo: HTMLElement): void {
+    this.renderer.setStyle(todo, 'opacity', 1);
+  }
+
+  public onDragStart(e: DragEvent): void {
+    e.dataTransfer!.setData('initID', (<HTMLElement>e.target!).id);
+    e.dataTransfer!.effectAllowed = 'move';
+  }
+
+  public onDragOver(e: DragEvent): void {
+    e.preventDefault();
+    e.dataTransfer!.dropEffect = 'move';
+  }
+
+  public onDrop(e: DragEvent, todo: HTMLElement): void {
+    e.preventDefault();
+    const draggedID = +e.dataTransfer!.getData('initID');
+    const droppedID = +(e as any).toElement.getAttribute('id');
+    this.renderer.setStyle(todo, 'opacity', 1);
+
+    [this.mockup[draggedID], this.mockup[droppedID]] = [this.mockup[droppedID], this.mockup[draggedID]];
   }
 
   private countLeftTodos(): void {
