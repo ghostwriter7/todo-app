40bb5877c7ea33954a95bbf5fb2604d92b5097d6
Hubert Rozplochowski <Hubert.Rozplochowski@createctrl.com>
2022-02-12 08:37:43 +0100

    refactored calendar into service + fetching calendar data from Firestore

---
+++ src/app/pages/todo/core/services/calendar.service.ts
@@ -0,0 +1,125 @@
+import { Injectable, OnInit } from '@angular/core';
+import { BehaviorSubject, ReplaySubject } from 'rxjs';
+import { AuthService } from '../../../auth/core/services/auth.service';
+import { Firestore, getFirestore, getDocs, query, where, collection } from 'firebase/firestore';
+import { app } from '../../../../core/libs/firebase';
+import { EventsService } from '../../../../core/services/events.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CalendarService implements OnInit {
+  private currentYear = new Date().getFullYear();
+  private currentMonth = new Date().getMonth();
+  private  calendarData: { [key:string]: any, date: number, isToday: boolean, hasPassed: boolean, total?: number, completed?: number }[] = [];
+  private currentYearSubject = new BehaviorSubject<number>(this.currentYear);
+  private currentMonthSubject = new BehaviorSubject<number>(this.currentMonth);
+  public calendarSubject = new BehaviorSubject<{ date: number, isToday: boolean, hasPassed: boolean, total?: number, completed?: number }[]>(this.calendarData);
+
+  private readonly db!: Firestore;
+
+  private monthData: any;
+
+  public currentYear$ = this.currentYearSubject.asObservable();
+  public currentMonth$ = this.currentMonthSubject.asObservable();
+
+  private startingGapSubject = new ReplaySubject<string>(1);
+  private endingGapSubject = new ReplaySubject<string>(1);
+  public startingGap$ = this.startingGapSubject.asObservable();
+  public endingGap$ = this.endingGapSubject.asObservable();
+
+  constructor(
+    private readonly _authService: AuthService,
+    private readonly _eventsService: EventsService
+  ) {
+    this.db = getFirestore(app);
+  }
+
+  ngOnInit(): void {
+  }
+
+  public fetchMonth(year: number, month: number): void {
+    setTimeout(()=>this._eventsService.startLoading(), 100);
+
+    const creator = this._authService.userSubject.getValue();
+
+    const monthsRef = collection(this.db, 'months');
+
+    const matchCreator = where('creator', '==', creator!.id);
+    const matchYear = where('year', '==', year);
+    const matchMonth = where('month', '==', month);
+
+    const q = query(monthsRef, matchCreator, matchYear, matchMonth);
+
+    getDocs(q)
+      .then((querySnapshot) => {
+        if (querySnapshot.docs.length) {
+          this.monthData = querySnapshot.docs[0].data();
+          const days = new Array(31).fill((0)).map((_, idx) => (idx + 1).toString());
+          for (let key in this.monthData) {
+            if (days.includes(key)) {
+              this.calendarData[+key - 1]['total'] = this.monthData[key].total;
+              this.calendarData[+key - 1]['completed'] = this.monthData[key].completed;
+
+              this.calendarSubject.next(this.calendarData);
+            }
+          }
+        }
+      })
+      .catch((err) => {
+        console.log(err);
+      })
+      .finally(() => this._eventsService.stopLoading());
+  }
+
+  public renderCalendar(): void {
+    const today = new Date();
+
+    const numberOfDays = new Date(this.currentYear, this.currentMonth + 1, 0).getDate();
+    let firstDay = new Date(this.currentYear, this.currentMonth, 1).getDay();
+    let lastDay = new Date(this.currentYear, this.currentMonth, numberOfDays).getDay();
+
+    firstDay = firstDay ? firstDay : 7;
+    lastDay = lastDay ? lastDay : 7;
+    this.calendarData = [];
+
+    for (let i = 1; i <= numberOfDays; i++) {
+      this.calendarData.push({
+        date: i,
+        isToday: false,
+        hasPassed: new Date(this.currentYear, this.currentMonth, i).getTime() < today.getTime() });
+    }
+
+    if (this.currentYear === today.getFullYear() && this.currentMonth === today.getMonth()) {
+      const currentDay = this.calendarData.find(tile => tile.date === today.getDate());
+      currentDay!.isToday = true;
+      currentDay!.hasPassed = false;
+    }
+
+    const startingGap = 100 - (7 - firstDay + 1) * (100 / 7) + '%';
+    const endingGap = 100 - (lastDay * (100 / 7)) + '%';
+
+    this.endingGapSubject.next(endingGap);
+    this.startingGapSubject.next(startingGap);
+    this.calendarSubject.next(this.calendarData);
+  }
+
+  public setPreviousYear(): void {
+    this.currentYear--;
+    this.currentYearSubject.next(this.currentYear);
+    this.renderCalendar();
+  }
+
+  public setNextYear(): void {
+    this.currentYear++;
+    this.currentYearSubject.next(this.currentYear);
+    this.renderCalendar();
+  }
+
+  public setMonth(month: number): void {
+    this.currentMonth = month;
+    this.currentMonthSubject.next(this.currentMonth);
+    this.renderCalendar();
+  }
+
+}
--- src/app/pages/todo/pages/calendar/calendar.component.ts
+++ src/app/pages/todo/pages/calendar/calendar.component.ts
@@ -1,76 +1,53 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { combineLatest } from 'rxjs';
+import { CalendarService } from '../../core/services/calendar.service';
 
 @Component({
   selector: 'app-calendar',
   templateUrl: './calendar.component.html',
   styleUrls: ['./calendar.component.scss']
 })
-export class CalendarComponent implements OnInit {
+export class CalendarComponent implements OnInit, AfterViewInit {
   public readonly months: string[] = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August',
     'September', 'October', 'November', 'December'];
 
-  public currentYear = new Date().getFullYear();
-  public currentMonth = new Date().getMonth();
 
-  public numberOfDays!: number;
-  public firstDay!: number;
-  public lastDay!: number;
-
-  public startingGap!: string;
-  public endingGap!: string;
-
-  public calendarData: { date: number, isToday: boolean, hasPassed: boolean }[] = [];
+public currentMonth!: number;
+public currentYear!: number;
 
   constructor(
     private router: Router,
+    public readonly _calendarService: CalendarService
   ) {}
 
   ngOnInit(): void {
-    this.renderCalendar();
+    combineLatest(
+      this._calendarService.currentMonth$,
+      this._calendarService.currentYear$,
+    ).subscribe(([month, year]) => {
+      this.currentYear = year;
+      this.currentMonth = month;
+      this._calendarService.renderCalendar();
+      this._calendarService.fetchMonth(year, month);
+    });
+
+  }
+
+  ngAfterViewInit() {
+
   }
 
   public onPrevYear(): void {
-    this.currentYear--;
-    this.renderCalendar();
+    this._calendarService.setPreviousYear();
   }
 
   public onNextYear(): void {
-    this.currentYear++;
-    this.renderCalendar();
+    this._calendarService.setNextYear();
   }
 
   public onMonthSelected(month: number): void {
-    this.currentMonth = month;
-    this.renderCalendar();
-  }
-
-  public renderCalendar(): void {
-    const today = new Date();
-
-    this.numberOfDays = new Date(this.currentYear, this.currentMonth + 1, 0).getDate();
-    this.firstDay = new Date(this.currentYear, this.currentMonth, 1).getDay();
-    this.lastDay = new Date(this.currentYear, this.currentMonth, this.numberOfDays).getDay();
-
-    this.firstDay = this.firstDay ? this.firstDay : 7;
-    this.lastDay = this.lastDay ? this.lastDay : 7;
-    this.calendarData = [];
-
-    for (let i = 1; i <= this.numberOfDays; i++) {
-      this.calendarData.push({
-        date: i,
-        isToday: false,
-        hasPassed: new Date(this.currentYear, this.currentMonth, i).getTime() < today.getTime() });
-    }
-
-    if (this.currentYear === today.getFullYear() && this.currentMonth === today.getMonth()) {
-      const currentDay = this.calendarData.find(tile => tile.date === today.getDate());
-      currentDay!.isToday = true;
-      currentDay!.hasPassed = false;
-    }
-
-    this.startingGap = 100 - (7 - this.firstDay + 1) * (100 / 7) + '%';
-    this.endingGap = 100 - (this.lastDay * (100 / 7)) + '%';
+    this._calendarService.setMonth(month);
   }
 
   onSelectDate(date: number): void {
